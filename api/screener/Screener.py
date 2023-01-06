e97b570353957cb158f8f14e089909eeb2867652
bigsaso <sal.logozzo@gmail.com>
2023-01-06 13:14:33 -0500

    Added sentiment to API

--- api/screener/Screener.py
+++ api/screener/Screener.py
@@ -4,6 +4,7 @@ from yahoo_fin.stock_info import get_data, tickers_sp500 as sp500
 
 from functions.gather_data import gather_data
 from screener.screener_algorithm import screener
+from indicators.sentiment import analyze_sentiment
 
 today = dt.date.today()
 last_year = (dt.datetime.now()-relativedelta(years=1))
@@ -27,7 +28,8 @@ def check_index(function):
         company = gather_data(data)
         company_ticker,company_status,company_streak,scan_date = screener(company)
         if company_status:
-            results.append({"Ticker": company_ticker, "Status": company_status, "Streak": company_streak, "Scan Date": scan_date})
+            sentiment,lt,warnings = analyze_sentiment(company_ticker)
+            results.append({"Ticker": company_ticker, "Status": company_status, "Streak": company_streak, "Scan Date": scan_date, "ST Sentiment": sentiment, "LT Analysis": lt, "Warnings": warnings})
     return results
 
 
@@ -37,7 +39,8 @@ def check_stock(ticker):
     company = gather_data(data)
     company_ticker,company_status,company_streak,scan_date = screener(company)
     if company_status:
-        results.append({"Ticker": company_ticker, "Status": company_status, "Streak": company_streak, "Scan Date": scan_date})
+        sentiment,lt,warnings = analyze_sentiment(company_ticker)
+        results.append({"Ticker": company_ticker, "Status": company_status, "Streak": company_streak, "Scan Date": scan_date, "ST Sentiment": sentiment, "LT Analysis": lt, "Warnings": warnings})
     return results
 
 # Check custom stock
@@ -45,5 +48,6 @@ def check_custom_stock(csv):
     company = gather_data(csv)
     company_ticker,company_status,company_streak,scan_date = screener(company)
     if company_status:
-        results.append({"Ticker": company_ticker, "Status": company_status, "Streak": company_streak, "Scan Date": scan_date})
+        sentiment,lt,warnings = analyze_sentiment(company_ticker)
+        results.append({"Ticker": company_ticker, "Status": company_status, "Streak": company_streak, "Scan Date": scan_date, "ST Sentiment": sentiment, "LT Analysis": lt, "Warnings": warnings})
     return results
