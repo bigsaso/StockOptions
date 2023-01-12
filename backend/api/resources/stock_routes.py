de186a2c15303b59ecec61b652a397a81c965316
bigsaso <sal.logozzo@gmail.com>
2023-01-12 11:59:18 -0500

    Removed S&P500 Routes

--- backend/api/resources/stock_routes.py
+++ backend/api/resources/stock_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from business.functions.checks import check_nasdaq100, check_sp500
 
-stock_routes = Blueprint('server_routes', __name__)
+stock_routes = Blueprint('stock_routes', __name__)
 
 # Initialize temp memory -> Will transfer to DB
 nasdaq100_result = []
@@ -20,17 +20,3 @@ def update_nasdaq100():
 def get_nasdaq100_info():
     global nasdaq100_result
     return nasdaq100_result
-
-# # API Endpoint to update S&P500 Screener Results
-# @stock_routes.post('/checkSp500')
-# def update_sp500():
-#     global sp500_result
-#     sp500_result = []
-#     sp500_result = check_sp500()
-#     return 'PASS'
-
-# # API Endpoint to get S&P500 Screener Results
-# @stock_routes.get('/getSp500')
-# def get_sp500_info():
-#     global sp500_result
-#     return sp500_result
